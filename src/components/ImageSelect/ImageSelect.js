affa3ba1b2961f03919815e7e82eb72ec43e90f7
Tony Braasch <tonybraasch@gmail.com>
2019-03-25 16:35:37 -0500

    add shuffle button for images

--- src/components/ImageSelect/ImageSelect.js
+++ src/components/ImageSelect/ImageSelect.js
@@ -10,12 +10,13 @@ class ImageSelect extends Component {
 
     componentDidMount(){
         this.props.dispatch({type: 'FETCH_IMAGES'});
-        this.props.dispatch({type: 'FETCH_USER'});
+        // this.props.dispatch({type: 'FETCH_USER'});
     }
 
  
     displayImages = () => {
-        console.log(this.props.images);
+        console.log('shuffle clicked');
+        
         const images = this.props.images;
         const shuffled = images.sort(() => 0.5 - Math.random());
         let selected = shuffled.slice(0, 6);
@@ -25,20 +26,17 @@ class ImageSelect extends Component {
 
     render() {
 
-        
-        
-        
         return (
             <>
             <div className="select-page-header">
                 <h2>Select Image</h2>
-                <p>Which image are you most drawn to in the present moment... click!</p>
-                <p>For more options click on shuffle below. Or if feeling inspired, choose upload to select your own image.</p>
+                    <p>Which image are you most drawn to in the present moment... click! 
+                        <br/>For more options click on shuffle below.</p>
             </div>
             <div className="images-grid-container">
-                {this.displayImages()}
-                    {/* <img src="images/a7fc3b296a575f9bcb1e9cf03f21feae.jpg" /> */}
+                {this.displayImages()}        
             </div>
+                <button onClick={() => this.props.dispatch({ type: 'FETCH_IMAGES' })} className="shuffle-button button">shuffle</button>
             </>
         );
     }
