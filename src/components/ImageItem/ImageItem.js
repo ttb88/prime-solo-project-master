ae99d8cefb2226d94d3774dde238f9c8ae267a42
Tony Braasch <tonybraasch@gmail.com>
2019-03-25 23:54:30 -0500

    add fade-in to images on image page

--- src/components/ImageItem/ImageItem.js
+++ src/components/ImageItem/ImageItem.js
@@ -29,10 +29,20 @@ class ImageItem extends Component {
         this.props.history.push("/genre");
     }
 
+    randomNumber = () => {
+        return Math.floor(Math.random() * 1.6) + 1;
+    }
+
+    randomNumberTwo = () => {
+        return Math.random() -.1;
+    }
+
+
     render() {
   
         return (
-            <div className={`image-grid-item${this.props.number}`}>
+            <div className={`image-reveal image-grid-item${this.props.number}`} style={{ animationDelay: `${this.randomNumberTwo()}s`,
+                animationName: this.props.animation, animationDuration: `${this.randomNumber()}s`}}>
                 <img src={this.props.image.image_path} alt={this.props.image.id} onClick={this.handleImageClick(this.props.image.id)} />
             </div>
         );
--- src/components/ImageSelect/ImageSelect.js
+++ src/components/ImageSelect/ImageSelect.js
@@ -2,41 +2,61 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ImageItem from '../ImageItem/ImageItem';
 import './ImageSelect.css';
-
-
+import { number } from 'prop-types';
 
 
 class ImageSelect extends Component {
 
-    componentDidMount(){
-        this.props.dispatch({type: 'FETCH_IMAGES'});
+    state = {
+        toggle: true,
+        animation: 'fadeIn',
+    }
+
+    componentDidMount() {
+        this.props.dispatch({ type: 'FETCH_IMAGES' });
         // this.props.dispatch({type: 'FETCH_USER'});
     }
 
- 
+
     displayImages = () => {
         console.log('shuffle clicked');
-        
         const images = this.props.images;
         const shuffled = images.sort(() => 0.5 - Math.random());
         let selected = shuffled.slice(0, 6);
         let number = 1
-        return selected.map(image => <ImageItem history={this.props.history} image={image} number={number++} key={image.id}/>)
+        return selected.map(image => <ImageItem history={this.props.history} animation={this.state.animation} image={image} number={number++} key={image.id} />)
     }
+
+    handleClick = () => {
+        this.props.dispatch({ type: 'FETCH_IMAGES' });
+        if (this.state.toggle) {
+            this.setState({
+                toggle: false,
+                animation: 'fadeInTwo',
+            })
+        } else {
+            this.setState({
+                toggle: true,
+                animation: 'fadeIn',
+            })
+        }
+    }
+
 
     render() {
 
         return (
             <>
-            <div className="select-page-header">
-                <h2>Select Image</h2>
-                    <p>Which image are you most drawn to in the present moment... click! 
-                        <br/>For more options click on shuffle below.</p>
-            </div>
-            <div className="images-grid-container">
-                {this.displayImages()}        
-            </div>
-                <button onClick={() => this.props.dispatch({ type: 'FETCH_IMAGES' })} className="shuffle-button button">shuffle</button>
+                <div className="select-page-header">
+                    <h2>Select Image</h2>
+                    <p>Which image are you most drawn to in the present moment... click!
+                        <br />For more options click on shuffle below.</p>
+                </div>
+                <div className="images-grid-container">
+                    {this.displayImages()}
+                </div>
+                {/* <button onClick={() => this.props.dispatch({ type: 'FETCH_IMAGES' })} className="shuffle-button button">shuffle</button> */}
+                <button onClick={this.handleClick} className="shuffle-button button">shuffle</button>
             </>
         );
     }
